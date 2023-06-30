ba41f01be6a743dc71ebb1fe66b35e66965c95f1
Aditya <aaditya1392@gmail.com>
2023-06-30 12:23:05 +0530

    set secure true in cookie

--- src/components/sendCookie.js
+++ src/components/sendCookie.js
@@ -1,8 +1,10 @@
 const sendCookie = (res, tokenName, token, expiry) => {
-    return res.cookie(tokenName, token, {
-        httpOnly: true,
-        maxAge: expiry
-    })
-}
 
-export default sendCookie
+	return res.cookie(tokenName, token, {
+		httpOnly: true,
+		maxAge: expiry,
+		secure: process.env.DEVELOPMENT === "true" ? false : true,
+	});
+};
+
+export default sendCookie;
