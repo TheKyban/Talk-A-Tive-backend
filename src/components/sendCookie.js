20196b5538049cefc6641a96fe3956d0bd5c9847
Aditya <aaditya1392@gmail.com>
2023-06-30 12:35:57 +0530

    secure cookie

--- src/components/sendCookie.js
+++ src/components/sendCookie.js
@@ -1,9 +1,9 @@
 const sendCookie = (res, tokenName, token, expiry) => {
-
 	return res.cookie(tokenName, token, {
-		httpOnly: true,
 		maxAge: expiry,
-		secure: process.env.DEVELOPMENT === "true" ? false : true,
+		// secure: process.env.DEVELOPMENT === "true" ? false : true,
+		secure: true,
+		httpOnly: true,
 	});
 };
 
